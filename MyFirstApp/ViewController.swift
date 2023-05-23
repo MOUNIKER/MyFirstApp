237cf4c8db450075167b4e18265195e5dcfdc023
MOUNIKER <v.s.mouniker@gmail.com>
2023-05-23 12:24:44 +0530

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Siva Mouniker  on 17/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            // Set the default background color to white
+            view.backgroundColor = .white
+        }
+        
+        override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+            super.traitCollectionDidChange(previousTraitCollection)
+            
+            // Set the background color to yellow in landscape orientation
+            if UIDevice.current.orientation.isLandscape {
+                view.backgroundColor = .yellow
+            }
+            
+            // Set the background color to green in portrait orientation
+            if UIDevice.current.orientation.isPortrait {
+                view.backgroundColor = .green
+            }
+        }
+    
+    
+
+
+}
+
